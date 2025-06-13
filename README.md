7c23d910fc0d5839bf361eda63825a7aa88a8925
Florian Fontan <dev@florian-fontan.fr>
2025-06-14 00:11:01 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# pace2025
-Submission for the PACE 2025 challenge
+# PACE 2025 challenge
+
+Submission for the PACE 2025 challenge.
+
+Compile:
+```shell
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release --parallel
+cmake --install build --config Release --prefix install
+```
+
+Run:
+```shell
+./install/bin/pace2025_ds_heuristic < heuristic_001.gr
+./install/bin/pace2025_hs_heuristic < heuristic_001.hgr
+```
---
+++ pace2025_ds_heuristic_main.cpp
@@ -0,0 +1,101 @@
+#include "setcoveringsolver/instance_builder.hpp"
+#include "setcoveringsolver/solution.hpp"
+#include "setcoveringsolver/algorithms/local_search_row_weighting.hpp"
+#include "setcoveringsolver/algorithms/large_neighborhood_search.hpp"
+#include "setcoveringsolver/algorithm_formatter.hpp"
+
+using namespace setcoveringsolver;
+
+Output optimize(
+        const Instance& instance,
+        std::mt19937_64& generator,
+        const Parameters& parameters)
+{
+    Output output(instance);
+    AlgorithmFormatter algorithm_formatter(parameters, output);
+    algorithm_formatter.start("Optimize");
+
+    // Reduction.
+    if (parameters.reduction_parameters.reduce) {
+        return solve_reduced_instance(
+                [&generator](
+                    const Instance& instance,
+                    const Parameters& parameters)
+                {
+                    return optimize(
+                            instance,
+                            generator,
+                            parameters);
+                },
+                instance,
+                parameters,
+                algorithm_formatter,
+                output);
+    }
+
+    algorithm_formatter.print_header();
+
+    if (!parameters.timer.needs_to_end()) {
+        LargeNeighborhoodSearchParameters lns_parameters;
+        lns_parameters.verbosity_level = 0;
+        lns_parameters.reduction_parameters.reduce = false;
+        lns_parameters.enable_new_solution_callback = parameters.enable_new_solution_callback;
+        lns_parameters.maximum_number_of_iterations_without_improvement
+            = 10 * instance.number_of_sets();
+        //lns_parameters.timer = parameters.timer;
+        lns_parameters.timer.set_time_limit(parameters.timer.remaining_time() / 2);
+        lns_parameters.goal = output.bound;
+        auto lns_output = large_neighborhood_search(instance, lns_parameters);
+        algorithm_formatter.update_solution(lns_output.solution, "lns");
+    }
+
+    if (!parameters.timer.needs_to_end()
+            && output.solution.cost() != output.bound) {
+        if (instance.compute_average_number_of_set_neighbors_estimate()
+                * instance.number_of_sets() < 2e7) {
+            LocalSearchRowWeighting2Parameters rwls_parameters;
+            rwls_parameters.verbosity_level = 0;
+            rwls_parameters.reduction_parameters.reduce = false;
+            rwls_parameters.enable_new_solution_callback = parameters.enable_new_solution_callback;
+            rwls_parameters.timer = parameters.timer;
+            auto rwls_output = local_search_row_weighting_2(instance, generator, &output.solution, rwls_parameters);
+            algorithm_formatter.update_solution(rwls_output.solution, "rwls");
+        } else {
+            LocalSearchRowWeighting1Parameters rwls_parameters;
+            rwls_parameters.verbosity_level = 0;
+            rwls_parameters.reduction_parameters.reduce = false;
+            rwls_parameters.enable_new_solution_callback = parameters.enable_new_solution_callback;
+            rwls_parameters.timer = parameters.timer;
+            auto rwls_output = local_search_row_weighting_1(instance, generator, &output.solution, rwls_parameters);
+            algorithm_formatter.update_solution(rwls_output.solution, "rwls");
+        }
+    }
+
+    algorithm_formatter.end();
+    return output;
+}
+
+int main(int argc, char *argv[])
+{
+    Parameters parameters;
+    parameters.verbosity_level = 0;
+    parameters.timer.set_time_limit(285);
+    parameters.reduction_parameters.timer.set_time_limit(150);
+    parameters.reduction_parameters.reduce = true;
+    parameters.reduction_parameters.remove_dominated = true;
+    parameters.enable_new_solution_callback = false;
+
+    // Build instance.
+    InstanceBuilder instance_builder;
+    instance_builder.read_pace2025_ds(stdin);
+    const Instance instance = instance_builder.build();
+
+    std::mt19937_64 generator(0);
+
+    // Run.
+    auto output = optimize(instance, generator, parameters);
+
+    output.solution.write_pace2025(std::cout);
+
+    return 0;
+}
